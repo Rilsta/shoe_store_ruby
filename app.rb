fbdcc16ff221914afd60d76c2b0b0b0b1f4f1ffd
Rilsta <rstarnes9@gmail.com>
2016-01-28 16:33:03 -0800

    add get requests to app.rb to navigate to store form

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require('pry')
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  erb(:index)
+end
+
+get('/stores') do
+  erb(:stores)
+end
+
+get('/stores/new') do
+  erb(:store_form)
+end
